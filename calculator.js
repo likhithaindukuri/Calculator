45243b5eaf5052b10a5e8abac8e538ad964f166e
Indukuri Likhitha <likhithaindukuri07@gmail.com>
2023-03-27 20:59:00 +0530

    Modified

--- calculator.js
+++ calculator.js
@@ -4,7 +4,7 @@ const express=require("express");
 const app=express();
 
 app.get("/",function(req,res){
-  res.sendFile("index.html");
+  console.log(__dirname);
 });
 
 app.listen(3000,function(){
